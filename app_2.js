e731d7ef801855ed7c56bfe0457320a87417c21e
Kiran Deep <kirandeep102030@gmail.com>
2022-01-27 20:32:35 +0530

    Added app_2.js to import cicle.js

---
+++ app_2.js
@@ -0,0 +1,7 @@
+const circle = require("./circle")
+
+const area = circle.circleArea(4)
+const circumference = circle.circleCircumference(5)
+const pi = circle.circlePI;
+
+console.log(`Area : ${area}\nCircumference : ${circumference}\nPI = ${pi}`)
