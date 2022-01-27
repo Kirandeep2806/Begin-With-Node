c872c2f234a5960f33cc831ac5f95a35396d0646
Kiran Deep <kirandeep102030@gmail.com>
2022-01-27 20:36:42 +0530

    Merged app_2 with app_1 and deleted app_2

--- app_1.js
+++ app_1.js
@@ -1,11 +1,20 @@
 // Let's import the square.js over here
-
 const square = require("./square.js")
+
+// Let's import the circle.js over here
+const circle = require("./circle")
+
+let area = circle.circleArea(4)
+let circumference = circle.circleCircumference(5)
+let pi = circle.circlePI;
+
+console.log(`Area : ${area}\nCircumference : ${circumference}\nPI = ${pi}`)
+
 
 // console.log(square)
 
-const perimeter = require("./square").perimeterCalculator(24.5);
-const area = square.areaCalculator(5.2); // We can write the above as like this too
-const pi = square.pi;
+let perimeter = require("./square").perimeterCalculator(24.5);
+area = square.areaCalculator(5.2); // We can write the above as like this too
+pi = square.pi;
 
 console.log(`Area : ${area}\nPerimeter : ${perimeter}\nPI : ${pi}`)
