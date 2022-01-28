f24d095c4ee84673312c12b75d580577c2bef205
Kiran Deep <kirandeep102030@gmail.com>
2022-01-28 12:38:30 +0530

    Added a clear explanation to exports v/s module.exports

--- exports vs module.exports.js
+++ exports vs module.exports.js
@@ -1,21 +1,28 @@
-const foo = () => console.log("Foo");
-const bar = () => console.log("Bar");
+exports.foo = () => console.log("Foo");
+exports.bar = () => console.log("Bar");
 
-exports = module.exports
-// Reassigning module.exports
-module.exports = {
+// Implicitly "module.exports = exports"
+// and the implicit return type is "module.exports" but not "exports"
+
+// This is sssigning via key-value, as it is reference type, we can do like this also as below
+
+// module.exports.foo = foo;
+// module.exports.bar = bar;
+
+// But we can't assign directly like, 
+
+/*
+exports = {
     foo: foo,
-    bar : bar
+    bar: bar
 }
+bcoz this make the reference type to be lost and as said at last module.exports will be returned i.e., {} in this case,
+So to make the above syntax work you can write a line,
 
-console.log(exports)
-console.log(module.exports)
+module.exports = exports
+*/
 
-module.exports = {}
-exports = module.exports
-// Assigning via key-value
-module.exports.foo = foo;
-module.exports.bar = bar;
-
-console.log(exports)
-console.log(module.exports)
+if(!module.parent) {
+    console.log("Exports has :", exports)
+    console.log("module.exports has :", module.exports)
+}
