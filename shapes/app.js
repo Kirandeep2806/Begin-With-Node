4edd47944c863002d4af0ce71c4c02e89068f7cb
Kiran Deep <kirandeep102030@gmail.com>
2022-01-30 21:21:43 +0530

    Installed and added code related to chalk package

--- shapes/app.js
+++ shapes/app.js
@@ -8,3 +8,12 @@ console.log(circle.circlePI)
 
 console.log(square.areaCalculator(4.6))
 console.log(square.perimeterCalculator(8.3))
+
+const chalk = require("chalk")
+const error = chalk.white.bold.italic.underline.bgRed
+const success = chalk.green
+const warning = chalk.white.bgHex("#de811d")
+console.log(chalk.blue("Water"))
+console.log(error("Error!"))
+console.log(success("Green Light"))
+console.log(warning("Khabardaar...!!"))
