143041414061d81fc53dfa711bcf6beb0bdfaea8
Kiran Deep <kirandeep102030@gmail.com>
2022-03-05 11:41:36 +0530

    Deleted unnecessary code in hoistingDemonstration

--- hoistingDemonstration.js
+++ hoistingDemonstration.js
@@ -6,9 +6,3 @@
 // Example 1
 console.log(a);
 var a; // replace the var with let or const to see the difference, let and const doen't allow hoisting
-
-// Example 2
-const b; // Try to uncomment this line to see the error.
-// You can just declare the variable and then assign it later. But for const, you have to assign the value at the time of declaration.
-
-console.log(b);
