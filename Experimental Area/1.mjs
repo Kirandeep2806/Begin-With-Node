1835dfc98a53b53c901efb1b074ca88142cc820f
Kiran Deep <kirandeep102030@gmail.com>
2022-04-12 22:48:25 +0530

    Added promises without Async

--- Experimental Area/1.mjs
+++ Experimental Area/1.mjs
@@ -1,21 +1,18 @@
 // import fetch from 'node-fetch';
 
-const { time } = require("console");
-
 // const promise = fetch("https://chandan-02.github.io/anime-facts-rest-api/");
 // promise.then((request) => console.log(request));
 
 const a = () => "A";
 const b = () => {
-    setTimeout(() => {
-        return "B";
-    }, 1000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve("B"), 1000);
+    });
 }
 const c = () => "C";
 
 const printContent = () => {
     console.log(a());
-    time.sleep(1000);
     console.log(b());
     console.log(c());
 }
