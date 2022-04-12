e0ae698bd70ef8feafbc22a252795746494d49db
Kiran Deep <kirandeep102030@gmail.com>
2022-04-12 22:39:39 +0530

    Code for use-case of async

--- Experimental Area/1.mjs
+++ Experimental Area/1.mjs
@@ -1,4 +1,23 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
-const promise = fetch("https://chandan-02.github.io/anime-facts-rest-api/");
-promise.then((request) => console.log(request));
+const { time } = require("console");
+
+// const promise = fetch("https://chandan-02.github.io/anime-facts-rest-api/");
+// promise.then((request) => console.log(request));
+
+const a = () => "A";
+const b = () => {
+    setTimeout(() => {
+        return "B";
+    }, 1000);
+}
+const c = () => "C";
+
+const printContent = () => {
+    console.log(a());
+    time.sleep(1000);
+    console.log(b());
+    console.log(c());
+}
+
+printContent();
