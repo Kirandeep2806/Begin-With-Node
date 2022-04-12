6e8ac03cd569a2da5a41f6f83f9bca3f23d7da86
Kiran Deep <kirandeep102030@gmail.com>
2022-04-12 23:03:55 +0530

    Added code to fetch JSON data from Web

--- Experimental Area/1.mjs
+++ Experimental Area/1.mjs
@@ -1,20 +1,37 @@
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
-// const promise = fetch("https://chandan-02.github.io/anime-facts-rest-api/");
-// promise.then((request) => console.log(request));
+const apiURL = "https://anime-facts-rest-api.herokuapp.com/api/v1";
 
-const a = () => "A";
-const b = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject("B"), 1000);
-    });
-}
-const c = () => "C";
-
-const printContent = async() => {
-    console.log(a());
-    console.log(await b().catch(err => console.log("Kiran")));
-    console.log(c());
+const getAnime = async () => {
+    // await can be applied on the function which is returning Promises, and takes care that not statements are executed before the Promise is resolved
+    const response = await fetch(`${apiURL}`);
+    // const data = response.json();
+    const data = await response.json();
+    // console.log(data);
+    return data;
+    // In the above 2 await call both fetch and response.json are returning Promises, so we can use await on both of them.
 }
 
-printContent();
+getAnime();
+
+console.log("hi");
+console.log("hi");
+console.log("hi");
+console.log("hi");
+console.log("hi");
+
+// const a = () => "A";
+// const b = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => reject("B"), 1000);
+//     });
+// }
+// const c = () => "C";
+
+// const printContent = async() => {
+//     console.log(a());
+//     console.log(await b().catch(err => console.log("Kiran")));
+//     console.log(c());
+// }
+
+// printContent();
