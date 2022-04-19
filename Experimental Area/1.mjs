61612c00b44994b9597f6be477c460518cc42c5f
Kirandeep2806 <kirandeep102030@gmail.com>
2022-04-19 22:45:37 +0530

    Response object usage

--- Experimental Area/1.mjs
+++ Experimental Area/1.mjs
@@ -1,11 +1,13 @@
 import fetch from 'node-fetch';
 
-const apiURL = "https://anime-facts-rest-api.herokuapp.com/api/v1";
+const apiURL = "https://anime-facts-rest-api.herokuapp.com/api/v";
 
 const getAnime = async () => {
     // await can be applied on the function which is returning Promises, and takes care that not statements are executed before the Promise is resolved
     const response = await fetch(`${apiURL}`);
     // const data = response.json();
+    console.log(response.status);
+    // Explore the above line to see what all are the properties of the response object
     const data = await response.json();
     // console.log(data);
     return data;
