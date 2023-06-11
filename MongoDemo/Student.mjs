be7f6dd4cff1e44feb92353992f142ec7d3ffc14
Kirandeep2806 <kirandeep102030@gmail.com>
2023-06-11 21:03:05 +0530

    Added Mongo Snippets

--- MongoDemo/Student.mjs
+++ MongoDemo/Student.mjs
@@ -1,7 +1,41 @@
 import mongoose from "mongoose";
 
+const AddressSchema = new mongoose.Schema({
+    street: String,
+    area: String,
+    pincode: BigInt
+})
+
 const StudentSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        default: "KD Bro"
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    age: {
+        required: true,
+        type: Number,
+        min: 1,
+        max: 100
+    },
+    address: AddressSchema,
+    email: {
+        type: String,
+        lowercase: true
+    },
+    mobileNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+        validate: {
+            validator: v => (Number.isInteger(v) && v.toString().length===10),
+            message: props => `${props.value} is not a mobile number`
+        }
+    }
 })
 
 export default mongoose.model("Student", StudentSchema);
--- MongoDemo/index.mjs
+++ MongoDemo/index.mjs
@@ -5,9 +5,27 @@ import Student from "./Student.mjs"
     await mongoose.connect("mongodb://127.0.0.1:27017/testdb")
     .then(() => console.log("Connection Established"));
 
-    await Student.create({ name: "Kiran Deep" })
+    await Student.create({ name: "Dileep sai",
+        dob: Date.now(),
+        address: { street: "Mudfort", area: "Hyderabad", pincode: 500003 },
+        email: "KIRANDEEP102030@gmail.com",
+        mobileNumber: 8132453442,
+        age: 64
+    })
     .then((data) => console.log("Data Inserted Successfully", data))
-    .catch((e) => console.log("Error while inserting the data"));
+    .catch((e) => console.log("Error while inserting the data", e.message));
 
+    // Aggregation Pipeline
+
+    let data = await Student.updateOne({ name: "Kiran Deep" }, {$set: { email: "KIRANDEEP102030@gmail.com" }})
+    console.log(data);
+
+    data = await Student.aggregate([{$match:{age:{$gt:0, $lt: 20}}}, {$count: "count"}])
+    console.log(data);
+
+    // Single-purpose aggregation
+
+    data = await Student.where("age").gt(0).lt(20).where("mobileNumber").equals(8106328099);
+    console.log(data);
 
 })().catch(e => console.log(e));
